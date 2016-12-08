e0428663a675aec0026b822c2a772b52be5ffb31
Seth J. Moore <seth@hsimosoftware.com>
2016-12-08 09:37:46 -0500

    Add Cycle.js for interactivity

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('This could say something cool.');
+import {run} from '@cycle/xstream-run';
+import {header, h1, h2, makeDOMDriver} from '@cycle/dom';
+
+function main(sources) {
+  const sinks = {
+    DOM: sources.DOM.select('.name').events('mouseover')
+      .map(() => "Highly Specific Infomation Modification and Organization")
+      .startWith('')
+      .map(name =>
+        header([
+          h1('.name', 'HSIMO Software, LLC'),
+          h2(name)
+        ])
+      )
+  };
+
+  return sinks;
+}
+
+run(main, { DOM: makeDOMDriver('#experiment')});
